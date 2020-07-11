17b4d9f0e0c599aff1cd24a3f8db2c0ba6f24767
Dhruv Dalsaniya <49398471+acedesyne@users.noreply.github.com>
2020-07-11 14:28:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function calculate() {
+  if (
+    [
+      $("#diam").val().length,
+      $("#len").val().length,
+      $("#num").val().length,
+    ].includes(0)
+  ) {
+    $("#output").text("36");
+    return;
+  }
+
+  const diam = $("#diam").val();
+  const len = $("#len").val();
+  const num = $("#num").val();
+
+  const res = Math.round((diam * 3.1428 * len * num) / 1000000);
+
+  $("#output").text(res);
+  navigator.clipboard.writeText(res);
+}
+
+$(document).on("keypress", function (e) {
+  if (e.which === 13) {
+    calculate();
+  }
+});
